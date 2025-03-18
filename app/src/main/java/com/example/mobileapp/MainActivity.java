e39a46705b0850401f0d41834f29ec20f5e1094d
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-03-18 21:53:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button nextButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, NextActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
