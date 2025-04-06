c42b79055d14620080c2ef3f87814cc1b5ca60a5
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-04-06 17:52:56 +0300

    add custom icon

--- app/src/main/java/com/example/mobileapp/SplashActivity.java
+++ app/src/main/java/com/example/mobileapp/SplashActivity.java
@@ -17,6 +17,6 @@ public class SplashActivity extends AppCompatActivity {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
-        }, 3000);
+        }, 1500);
     }
 }
